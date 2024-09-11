d1613a33a810d0971851fab18f82639e88061709
Zhu Ze Feng <zhusandro@gmail.com>
2024-09-11 17:22:39 -0300

    Testes com listas

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,19 @@
+///<reference types="cypress"/>
+import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade: Produtos', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar produto da lista ', () => {
+        cy.get('.product-block > .caption > .meta > .infor > .name > a')
+            //.first()
+            //.last()
+            //.eq(2)
+            .contains('Aero Daily Fitness Tee')
+            .click()
+        cy.get('#tab-title-description > a').should('contain' , 'Descrição')
+    });
+});
