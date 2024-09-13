f49e7b2cb6e2edbe5a41dbac49b0a0704806fab8
Zhu Ze Feng <zhusandro@gmail.com>
2024-09-13 10:05:50 -0300

    page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,19 +1,30 @@
 ///<reference types="cypress"/>
 import { faker } from '@faker-js/faker';
+import produtosPage from '../../support/page-objects/produtos.page';
+
 
 describe('Funcionalidade: Produtos', () => {
     
     beforeEach(() => {
-        cy.visit('produtos/')
+        produtosPage.visitarUrl()
     });
 
     it('Deve selecionar produto da lista ', () => {
-        cy.get('.product-block > .caption > .meta > .infor > .name > a')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Aero Daily Fitness Tee')
-            .click()
-        cy.get('#tab-title-description > a').should('contain' , 'Descrição')
+        produtosPage.buscarProdutoLista('Aero Daily Fitness Tee')
+        cy.get('.product_title').should('exist')
+    });
+
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Abominable Hoodie'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain' , produto)
+    });
+
+    it('Deve visitar a pagina do produto', () => {
+        
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        
     });
 });
