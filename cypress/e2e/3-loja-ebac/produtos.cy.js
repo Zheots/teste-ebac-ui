b59611df0f6a59b963b459b7c612cf299eec8bce
Zhu Ze Feng <zhusandro@gmail.com>
2024-09-13 18:46:18 -0300

    massa de dados

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -14,17 +14,34 @@ describe('Funcionalidade: Produtos', () => {
         cy.get('.product_title').should('exist')
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Abominable Hoodie'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain' , produto)
     });
 
     it('Deve visitar a pagina do produto', () => {
-        
+        let produto = 'Apollo Running Short'
+        produtosPage.visitarProduto(produto)
+        cy.get('.product_title').should('contain' , produto)
     });
 
     it('Deve adicionar produto ao carrinho', () => {
+        let produto = 'Abominable Hoodie'
+        let tamanho = 'L'
+        let cor = 'Blue'
+        let quantidade = 2
+        produtosPage.buscarProduto(produto)
+        produtosPage.addProdutoCarrinho(tamanho , cor , quantidade)
+        cy.get('.woocommerce-message').should('contain' , quantidade + ' × “' + produto)
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {   
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[1].nomeProduto)
+            produtosPage.addProdutoCarrinho(dados[1].tamanho, dados[1].cor , dados[1].quantidade)
+            cy.get('.woocommerce-message').should('contain' , dados[1].quantidade + ' × “' + dados[1].nomeProduto)
+        })
         
     });
 });
