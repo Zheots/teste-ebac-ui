cff371944a395ccb825b7f93890b0dab5df706d7
Zhu Ze Feng <zhusandro@gmail.com>
2024-09-14 11:22:08 -0300

    mudanca no gitiginore

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://lojaebac.ebaconline.art.br/'
+    baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    video: true,
   },
+  projectId: "uo5j98"
 });
